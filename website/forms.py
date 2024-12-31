c6e3eab719d0157c78efd17f8853090289d7865c
unknown <gideonaddai123@outlook.com>
2025-01-01 00:21:35 +1200

    correct regstration setup

--- website/forms.py
+++ website/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django import forms
 
 
-class SignupForm(UserCreationForm):
+class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class' : 'form-control', 'placeholder':'Email Address' }))
     first_name = forms.CharField(label="",max_length=100, widget=forms.TextInput(attrs={'class' : 'form-control', 'placeholder':'Frist Name' }))
     last_name = forms.CharField(label="",max_length=100, widget=forms.TextInput(attrs={'class' : 'form-control', 'placeholder':'last Name' }))
